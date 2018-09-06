fd5c2b249f84d2cbf3dab1a7d9d7c81c2a9a5d2f
Monica Kalje <monkalje@in.ibm.com>
2018-09-06 16:48:48 +0530

    test commit

--- iOS/POCRegistration/POCRegistration/ViewController.swift
+++ iOS/POCRegistration/POCRegistration/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     let titleWidth = 300
     let titleHeight = 30
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
